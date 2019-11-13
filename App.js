bde18f4ea1b1fbf1168adf9c7b475a775c078f19
pervezn <nidapervez2020@u.northwestern.edu>
2019-11-13 13:32:27 -0600

    hooks

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import React from 'react';
+'use strict';
+import React, { PureComponent, useState, useEffect } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Permissions from 'react-native-permissions';
 
-import CameraPage from './src/camera.page';
+const CameraApp = () => {
+    let [flash, setFlash] = useState('off')
+    let [zoom, setZoom] = useState(0)
+    let [autoFocus, setAutoFocus] = useState('on')
+    let [depth, setDepth] = useState(0)
+    let [type, setType] = useState('back')
+    let [permission, setPermission] = useState('undetermined')
+    let cameraRef = useRef(null)
+    useEffect(() => {
+        Permissions.check('photo').then(response => {
+        // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+        setPermission(response);
+        });
+    }, []);
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <CameraPage />
-        );
+    toggleFlash = () => {
+        setFlash(flashModeOrder[flash])
+    }
+    zoomOut = () =>  {
+        setZoom(zoom - 0.1 < 0 ? 0 : zoom - 0.1)
+    }
+    zoomIn = () =>  {    
+        setZoom(zoom + 0.1 > 1 ? 1 : zoom + 0.1);
+    }
+    takePicture = async() => {
+        if (cameraRef) {
+            const options = { quality: 0.5, base64: true };
+            const data = await cameraRef.current.takePictureAsync(soptions);
+            console.log(data.uri);  
+        }
     };
-};
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={cameraRef}
+          style={styles.preview}
+          type={type}
+          flashMode={flash}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={takePicture} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+}
+
+// import React from 'react';
+
+// import CameraPage from './src/camera.page';
+
+// export default class App extends React.Component {
+//     render() {
+//         return (
+//             <CameraPage />
+//         );
+//     };
+// };
